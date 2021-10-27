fadc61fc70dfdd5461fd4b80971c976a34750418
Andrea Parisi <andreaparisi4502@gmail.com>
2021-10-27 19:10:51 +0200

    commit first version

---
+++ js/script.js
@@ -0,0 +1,103 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
+
+let imageCont = document.querySelector(".img-container");
+let thumbCont = document.querySelector(".thumb-container");
+
+let image="";
+let thumb="";
+
+
+for(let i = 0; i < items.length; i++){
+
+    image += `
+        <div class="image">
+            <img src="${items[i]}" alt="">
+        </div>
+        <div class="title">
+            ${title[i]}
+        </div>
+        <div class="text">
+            ${text[i]}
+        </div>
+    `;
+
+    thumb += `
+
+        <div class="thumb">
+            <img src="${items[i]}" alt="${title[i]}">
+        </div>
+
+    `;
+}
+    
+imageCont.innerHTML += image;
+document.getElementsByClassName("image")[0].classList.add("active");
+document.getElementsByClassName("title")[0].classList.add("active");
+document.getElementsByClassName("text")[0].classList.add("active");
+
+thumbCont.innerHTML += thumb;
+document.getElementsByClassName("thumb")[0].classList.add("active");
+
+let active = 0;
+
+document.querySelector(".next-arrow").addEventListener("click", function(){
+
+    active ++;
+
+    document.querySelector(".image.active").classList.remove("active");
+    document.getElementsByClassName("image")[active].classList.add("active");
+
+    document.querySelector(".title.active").classList.remove("active");
+    document.getElementsByClassName("title")[active].classList.add("active");
+
+    document.querySelector(".text.active").classList.remove("active");
+    document.getElementsByClassName("text")[active].classList.add("active");
+
+
+    document.querySelector(".thumb").classList.remove("active");
+    document.getElementsByClassName("thumb")[active].classList.add("active");
+    
+});
+
+
+document.querySelector(".prev-arrow").addEventListener("click", function(){
+
+    active --;
+
+    document.querySelector(".image.active").classList.remove("active");
+    document.getElementsByClassName("image")[active].classList.add("active");
+
+    document.querySelector(".title.active").classList.remove("active");
+    document.getElementsByClassName("title")[active].classList.add("active");
+
+    document.querySelector(".text.active").classList.remove("active");
+    document.getElementsByClassName("text")[active].classList.add("active");
+
+
+    document.querySelector(".thumb").classList.remove("active");
+    document.getElementsByClassName("thumb")[active].classList.add("active");
+    
+});
